e08ec399f6bfd410a3041a640a337a14c374c0c2
renandmae <renandmae@gmail.com>
2026-08-05 12:53:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15072,7 +15072,6 @@ function salvar(id){
 
         if(r.ok){
             alert("Salvo")
-            # tire o hashtag e comentário daqui, o location.reload serve para manter a pagina ao salvar e apertar F5. location.reload();
         }
         else{
             alert("Erro")
